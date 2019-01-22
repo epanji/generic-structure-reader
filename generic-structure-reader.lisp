79d830839effbc281dcb774a94be2b00e0c9a303
Panji Kusuma <epanji@gmail.com>
2019-01-22 07:51:57 +0700

    Add better error message
    - Add function slot-reader-expected-type
    - Add condition replace-slot-reader-error
    - Use both in macro define-generic-structure-reader

--- generic-structure-reader.lisp
+++ generic-structure-reader.lisp
@@ -4,6 +4,25 @@
 
 (defvar *structure-reader-functions* (make-hash-table))
 
+(defun slot-reader-expected-type (function)
+  (handler-case (funcall (typecase function
+                           (function function)
+                           (symbol (symbol-function function))
+                           (t nil))
+                         nil)
+    (type-error (c) (type-error-expected-type c))
+    (undefined-function ())))
+
+(define-condition replace-slot-reader-error (error)
+  ((function :initarg :function :reader replace-slot-reader-error-function)
+   (structure :initarg :structure :reader replace-slot-reader-error-structure))
+  (:report (lambda (condition stream)
+             (let ((function-name (replace-slot-reader-error-function condition))
+                   (structure-name (replace-slot-reader-error-structure condition)))
+               (format stream "Invalid ~(~a~) ~a for ~(~a~) ~a."
+                       (type-of (symbol-function function-name)) function-name
+                       (type-of (find-class structure-name nil)) structure-name)))))
+
 (defmacro structure-reader-function (name)
   `(gethash ,name *structure-reader-functions*))
 
@@ -11,17 +30,18 @@
   ;; Ensure type of FUNCTION-NAME and STRUCTURE-NAME related.
   (when (and (eql (type-of (symbol-function function-name)) 'function)
              (eql (type-of (find-class structure-name nil)) 'structure-class)
-             ;; Provide nil to get expected type from error.
-             (eql (handler-case (funcall (symbol-function function-name) nil)
-                    (type-error (c) (type-error-expected-type c)))
-                  structure-name))
+             (eql (slot-reader-expected-type function-name) structure-name))
     ;; Register original function.
     (setf (structure-reader-function function-name)
           (symbol-function function-name))
     ;; Remove function definition.
     (fmakunbound function-name))
-  ;; Create or [Re]define generic function.
-  `(defgeneric ,function-name (,structure-name)
-     (:method ((instance ,structure-name))
-       (funcall (structure-reader-function ',function-name) instance))
-     ,@body))
+  ;; Define or redefine generic function.
+  ;; Since slot-reader-expected-type did not work for method,
+  ;; STRUCTURE-NAME is needed for redefine generic function.
+  (if (eql (slot-reader-expected-type (structure-reader-function function-name)) structure-name)
+      `(defgeneric ,function-name (,structure-name)
+         (:method ((instance ,structure-name))
+           (funcall (structure-reader-function ',function-name) instance))
+         ,@body)
+      (error 'replace-slot-reader-error :function function-name :structure structure-name)))
