085d735fd5732951e0c5978b375b0240271b966e
Panji Kusuma <epanji@gmail.com>
2019-01-23 10:26:01 +0700

    Export condition

--- package.lisp
+++ package.lisp
@@ -3,4 +3,5 @@
 (defpackage #:generic-structure-reader
   (:use #:common-lisp)
   (:export #:define-generic-structure-reader
+           #:replace-slot-reader-error
            #:structure-reader-function))
