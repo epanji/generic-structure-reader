54f625fb27c3b8b36230c91d13ed5fb619922db9
Panji Kusuma <epanji@gmail.com>
2019-01-23 10:25:06 +0700

    Tidying tests

--- generic-structure-reader-tests.lisp
+++ generic-structure-reader-tests.lisp
@@ -5,20 +5,22 @@
   (:export #:run-suite-tests))
 (in-package #:generic-structure-reader/tests)
 
-(defmacro with-dummy-data (ignored &body body)
-  (defstruct foo a b)
-  (defstruct bar b c)
-  (defstruct baz z)
-  `(let ((foo (make-foo :a 1 :b 2))
-         (bar (make-bar :b 11 :c 22))
-         (baz (make-baz :z 111)))
-     (declare (ignore ,@ignored))
-     (handler-bind ((warning (lambda (c)
-                               (declare (ignore c))
-                               (muffle-warning))))
-       (define-generic-structure-reader foo-a (foo))
-       (define-generic-structure-reader bar-b (bar))
-       ,@body)))
+(defstruct foo a b)
+(defstruct bar b c)
+(defstruct baz z)
+
+(defmacro with-suppress-warning (&body body)
+  `(handler-bind ((warning (lambda (condition)
+                             (declare (ignore condition))
+                             (muffle-warning))))
+     ,@body))
+
+(def-fixture dummy-data ()
+  (let ((foo (make-foo :a 1 :b 2))
+        (bar (make-bar :b 11 :c 22))
+        (baz (make-baz :z 111)))
+    (declare (ignorable foo bar baz))
+    (&body)))
 
 (defun run-suite-tests ()
   (run! 'generic-structure-reader-suite))
@@ -28,35 +30,42 @@
 (in-suite generic-structure-reader-suite)
 
 (test replace-structure-reader-with-generic-function
-  (with-dummy-data (foo bar)
-    (is (typep (function foo-a) 'standard-generic-function))
-    (is (typep (function bar-b) 'standard-generic-function))
-    (is (typep (structure-reader-function 'foo-a) 'function))
-    (is (typep (structure-reader-function 'bar-b) 'function))))
+  (with-suppress-warning
+    (define-generic-structure-reader foo-a (foo))
+    (define-generic-structure-reader bar-b (bar)))
+  (is (typep (function foo-a) 'standard-generic-function))
+  (is (typep (function bar-b) 'standard-generic-function))
+  (is (typep (structure-reader-function 'foo-a) 'function))
+  (is (typep (structure-reader-function 'bar-b) 'function)))
 
-(test adding-method-for-structure-reader
-  (with-dummy-data ()
+(test (adding-method-for-structure-reader
+       :depends-on replace-structure-reader-with-generic-function)
+  (with-suppress-warning
     (defmethod foo-a ((object null)) 100)
-    (defmethod bar-b ((object null)) 200)
+    (defmethod bar-b ((object null)) 200))
+  (with-fixture dummy-data ()
     (is (= (foo-a nil) 100))
     (is (= (foo-a foo) 1))
     (is (= (bar-b nil) 200))
     (is (= (bar-b bar) 11))))
 
-(test access-unreplaced-structure-reader
-  (with-dummy-data ()
+(test (access-unreplaced-structure-reader
+       :depends-on adding-method-for-structure-reader)
+  (with-fixture dummy-data ()
     (is (= (foo-b foo) 2))
     (is (= (bar-c bar) 22))))
 
-(test able-to-change-values
-  (with-dummy-data ()
+(test (able-to-change-values
+       :depends-on adding-method-for-structure-reader)
+  (with-fixture dummy-data ()
     (is (setf (foo-a foo) 1))
     (is (setf (foo-b foo) 2))
     (is (setf (bar-b bar) 3))
     (is (setf (bar-c bar) 4))))
 
-(test able-to-use-with-slots-and-accessors
-  (with-dummy-data ()
+(test (able-to-use-with-slots-and-accessors
+       :depends-on adding-method-for-structure-reader)
+  (with-fixture dummy-data ()
     (with-slots (a b) foo
       (is (= a 1))
       (is (= b 2)))
@@ -65,12 +74,11 @@
       (is (= c 22)))))
 
 (test mismatch-between-structure-and-reader
-  (with-dummy-data (foo bar)
-    ;; Expecting error because slot z does not exists in bar.
-    (handler-case (define-generic-structure-reader baz-z (bar))
-      (error () (is (and)))
-      (:no-error () (is (or))))
-    (define-generic-structure-reader baz-z (baz))
+  (with-fixture dummy-data ()
+    (with-suppress-warning
+      ;; Expecting error because slot z does not exists in bar.
+      (signals error (define-generic-structure-reader baz-z (bar)))
+      (define-generic-structure-reader baz-z (baz)))
     (is (typep (function baz-z) 'standard-generic-function))
     (is (typep (structure-reader-function 'baz-z) 'function))
     (is (= (baz-z baz) 111))))
