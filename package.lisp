9dbd087958894730f0c1c10cc46b9345f6f4dfc3
Panji Kusuma <epanji@gmail.com>
2020-08-17 15:46:16 +0700

    Export revoke functions

--- package.lisp
+++ package.lisp
@@ -1,7 +1,9 @@
 ;;;; package.lisp
 
-(defpackage #:generic-structure-reader
+(defpackage :generic-structure-reader
   (:use #:common-lisp)
   (:export #:define-generic-structure-reader
            #:replace-slot-reader-error
-           #:structure-reader-function))
+           #:structure-reader-function
+           #:revoke-generic-structure-reader
+           #:revoke-generic-structure-readers))
